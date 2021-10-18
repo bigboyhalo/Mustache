62c7ab98b7e43a5cd535944e67d2b9b249d29a77
bigboyhalo <88926500+bigboyhalo@users.noreply.github.com>
2021-10-18 20:32:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+nosex=0;
+nosey=0;
+function preload(){
+clownnose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function gotPoses(results){
+    if (results.length>0) {
+        nosex=results[0].pose.nose.x-15;
+        nosey=results[0].pose.nose.y-15;
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(clownnose,nosex,nosey,30,30);
+}
+
+function takesnapshot(){
+    save("myfilter.png");
+}
